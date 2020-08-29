a00d361bd069641cc28abfc67c29ecdf9df9aa98
Achraf Elhamzaoui <achrfelh22@gmail.com>
2020-08-29 15:32:35 +0100

    setup template engine and basic layout

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 
-const connectDB = require("./config/db");
+const connectDB = require('./config/db');
 
 // Load Config
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 connectDB();
 
 const app = express();
 
 // Logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
+// View engine
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
