8894b327a326799aca568e19b096a6d98d2cc2bf
Achraf Elhamzaoui <achrfelh22@gmail.com>
2020-09-01 02:24:16 +0100

    Method override for DELETE requests

--- routes/stories.js
+++ routes/stories.js
@@ -8,8 +8,13 @@ const routes = express.Router();
 // @desc  Show add Page
 // @route Get /stories/add
 
-routes.get('/add', ensureAuth, (req, res) => {
-  res.render('stories/add');
+routes.get('/add', ensureAuth, async (req, res) => {
+  try {
+    res.render('stories/add');
+  } catch (err) {
+    console.error(err);
+    res.render('error/500');
+  }
 });
 
 // @desc  Process add form
@@ -49,17 +54,22 @@ routes.get('/', ensureAuth, async (req, res) => {
 // @route Get /stories/edit/:id
 
 routes.get('/edit/:id', ensureAuth, async (req, res) => {
-  const story = await Story.findOne({ _id: req.params.id }).lean();
+  try {
+    const story = await Story.findOne({ _id: req.params.id }).lean();
 
-  if (!story) {
-    res.render('error/404');
-  }
-  if (story.user != req.user.id) {
-    res.redirect('/stories');
-  } else {
-    res.render('stories/edit', {
-      story,
-    });
+    if (!story) {
+      res.render('error/404');
+    }
+    if (story.user != req.user.id) {
+      res.redirect('/stories');
+    } else {
+      res.render('stories/edit', {
+        story,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render('error/500');
   }
 });
 
@@ -67,19 +77,37 @@ routes.get('/edit/:id', ensureAuth, async (req, res) => {
 // @route PUT /stories/:id
 
 routes.put('/:id', ensureAuth, async (req, res) => {
-  let story = await Story.findOne({ _id: req.params.id }).lean();
-  if (!story) {
-    res.render('stories/404');
-  }
+  try {
+    let story = await Story.findOne({ _id: req.params.id }).lean();
+    if (!story) {
+      res.render('stories/404');
+    }
 
-  if (story.user != req.user.id) {
-    res.redirect('/stories');
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    if (story.user != req.user.id) {
+      res.redirect('/stories');
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      res.redirect('/dashboard');
+    }
+  } catch (err) {
+    console.error(err);
+    res.render('error/500');
+  }
+});
+
+// @desc  delete Story
+// @route DELETE /stories/:id
+
+routes.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id });
     res.redirect('/dashboard');
+  } catch (error) {
+    console.error(err);
+    res.render('error/500');
   }
 });
 
